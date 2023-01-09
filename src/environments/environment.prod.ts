a7a5cfb9c1cf2ba2f6448bc90e2389b97330efdb
Edvin <e.tuzlakovic@gmail.com>
2023-01-09 02:14:41 +0100

    env.prod url change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://edvin.com:4500',
+  baseUrl: 'http://localhost:5050',
   serverSpringURL: 'http://edvin.com:8055/api/v1',
   nasaServerAPI: 'https://api.nasa.gov/planetary/apod',
   API_KEY: 'KVoY0gWyBDNwNfq6SMLOCaP22jLzUf3QIFLRrn2F'
